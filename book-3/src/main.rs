f3cd3f6d57bea25ea4fbd4fd0ad9b1804b085f8a
erics <ericsutton6@gmail.com>
2023-12-06 21:19:38 -0600

    completed book section 3.1

--- book-3/src/main.rs
+++ book-3/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // Book section 3.1
+    // let mut x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is now {x}");
+    //
+    // const THREE_HOURS_IN_SECONDS: u32 = 3*60*60;
+    // let x = 5;
+    // let x = x + 1;
+    // {
+    //     let x = x + 3;
+    //     println!("The value of x in this scope is {x}");
+    // }
+    // println!("The value of x in this scope is {x}");
+
+    // Book section 3.2
+
 }
